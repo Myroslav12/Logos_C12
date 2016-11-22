e996a840ecaf002d5236929dc80f9f1c9384e3bc
Myroslav12 <myroslav98@gmail.com>
2016-11-22 21:00:17 +0200

    oo2o3

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  int n1 = int.Parse(Console.ReadLine());
+            //   int n2 = int.Parse(Console.ReadLine());
+            //  int oper = int.Parse(Console.ReadLine());
+
+            //switch (oper)
+            /*    {
+                case 1:
+                    Console.WriteLine(n1 * n2);
+                    break;
+
+                case 2:
+                    Console.WriteLine(n1 / n2);
+                    break;
+
+                case 3:
+                    Console.WriteLine(n1 + n2);
+                    break;
+
+                case 4:
+                    Console.WriteLine(n1 - n2);
+                    break;
+
+            };   */
+
+            //            int a1 = int.Parse(Console.ReadLine());
+            //          if (a1 % 4 == 0) { 
+            //              Console.WriteLine("Visokosniy");
+            //       }
+
+
+
+
+
+
+
+            int y1 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            int y3 = int.Parse(Console.ReadLine());
+            if (y1 + y2 > y3 && y1 + y3 > y2 && y2 + y3 > y1 )
+            {
+                Console.WriteLine("Good");
+
+
+            }
+
+
+
+        }
+
+
+    }
+}
